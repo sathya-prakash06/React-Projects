ea203da0f8e6aa7167ac07def688897a7a83b11a
Sathya <sathyaprakash.sp.6@gmail.com>
2022-05-13 09:28:48 +0530

    birthday remainder

--- 01.birthday-remainder/src/App.js
+++ 01.birthday-remainder/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+
+  const handleClear = () => {
+    setPeople([]);
+  };
+
+  return (
+    <main>
+      <section className="container">
+        <h2>0 birthdays today</h2>;
+        <List people={people} />
+        <button onClick={handleClear}> clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
